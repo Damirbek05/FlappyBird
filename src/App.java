975f9cfa64e73c244567fdb2a88a3b3a31099536
Damirbek05 <124022133+Damirbek05@users.noreply.github.com>
2024-02-12 21:13:41 +0600

    Set up JFrame

--- src/App.java
+++ src/App.java
@@ -1,7 +1,16 @@
 import javax.swing.*;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
